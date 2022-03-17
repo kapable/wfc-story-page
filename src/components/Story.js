7312569d89c64f9a9a3ccea881cf5768671afe7f
kapable <skjoldenstore@gmail.com>
2022-03-17 18:14:41 +0900

    add images and deploy to github page

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,28 +1,23 @@
 import React, { Fragment, useCallback, useState } from 'react';
-import { Collapse, UnmountClosed } from 'react-collapse';
-import WHOLESTORY from '../images/whole_story.png';
+import { Collapse } from 'react-collapse';
+import STORYTOP from '../images/story_top.png';
+import STORYBOTTOM from '../images/story_bottom.png';
 import STORYFOX from '../images/story_fox.png';
 import UPARROW from '../images/up_arrow.png';
 import DOWNARROW from '../images/down_arrow.png';
 
 function Story() {
     const [opened, setOpened] = useState(false);
-    const [coverHeight, setCoverHeight] = useState(window.screen.width > 600 ? "57rem" : "15rem");
     const onCollapseButtonClick = useCallback(() => {
         setOpened(!opened);
-        if(window.screen.width > 600) {
-            setCoverHeight("57rem");
-        } else {
-            setCoverHeight("15rem");
-        }
     }, [opened]);
 
     return (
         <Fragment>
             <div style={{position: "relative"}}>
-                <div style={{ width: "100%", height: 'auto', backgroundColor:"white"}}>111</div>
+                    <img src={STORYTOP} alt="story-img" className='story-img' />
                 <Collapse isOpened={opened}>
-                    <img src={WHOLESTORY} alt="story-img" className='story-img' />
+                    <img src={STORYBOTTOM} alt="story-img" className='story-img' />
                     <img src={STORYFOX} alt="story-fox-img" className='story-fox-img' />
                     <img src={opened ? UPARROW : DOWNARROW} alt="arrow" className='story-arrow' onClick={onCollapseButtonClick}/>
                 </Collapse>
