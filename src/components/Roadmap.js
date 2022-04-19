ba166f7e57f67babc34a149f46d1ff4b4ad9746e
kapable <skjoldenstore@gmail.com>
2022-04-19 15:57:39 +0900

    fix roadmap page for responsive

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ROADMAP from '../images/roadmap.png';
+import React, { useCallback, useState, useEffect } from 'react';
+import { ROADMAPSM, ROADMAPLG } from '../images';
 
 function Main() {
+    const roadmapRenderer = useCallback(() => {
+        if(window.screen.width < 1024) {
+            return (
+                <img src={ROADMAPSM} alt="roadmap-contents" className='roadmap-contents' />
+            )
+        } else {
+            return (
+                <img src={ROADMAPLG} alt="roadmap-contents" className='roadmap-contents' />
+            )
+        }
+    }, []);
+
     return (
-        <img src={ROADMAP} alt="roadmap-img" className='roadmap-img' />
+        <div className='roadmap-div'>
+            <div className='roadmap-background-container'>
+                <p className='roadmap-title'>ROAD<span className='roadmap-title-bold'>MAP</span></p>
+                {roadmapRenderer()}
+            </div>
+        </div>
     );
 };
 
