56f78bfd92b39019e967382e2ead27957b4c35a2
kapable <skjoldenstore@gmail.com>
2022-04-19 10:12:42 +0900

    fix css for responsive into team section

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import TEAMIMG from '../images/team.png';
+import { JACE, JACOB, JAMIE, LOUIS, SEEYONG } from '../images';
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css';
 
 function Main() {
     return (
-        <img src={TEAMIMG} alt="team-img" className='team-img' />
+        <div className='team-div'>
+            <div className='team-background-container'>
+                <p className='team-title'>W.F.C<br /><span className='team-title-bold'>TEAM</span></p>
+                <Row gutter={{ xs: 4, sm: 48, md: 48, lg: 56, xl: 160, xxl:180}} justify='center' className='team-member-row'>
+                    <Col xs={12} sm={8} md={6} lg={4} xl={4}>
+                        <p className='team-member-name'>JACE</p>
+                        <img className='team-member-avatar' src={JACE} alt="JACE" />
+                        <p className='team-member-title'>Project<br />director</p>
+                    </Col>
+                    <Col xs={12} sm={8} md={6} lg={4} xl={4}>
+                        <p className='team-member-name'>JAMIE</p>
+                        <img className='team-member-avatar' src={JAMIE} alt="JAMIE" />
+                        <p className='team-member-title'>Project<br />manager</p>
+                    </Col>
+                    <Col xs={12} sm={8} md={6} lg={4} xl={4}>
+                        <p className='team-member-name'>SEEYONG</p>
+                        <img className='team-member-avatar' src={SEEYONG} alt="SEEYONG" />
+                        <p className='team-member-title'>Developer</p>
+                    </Col>
+                    <Col xs={12} sm={8} md={6} lg={4} xl={4}>
+                        <p className='team-member-name'>LOUIS</p>
+                        <img className='team-member-avatar' src={LOUIS} alt="LOUIS" />
+                        <p className='team-member-title'>Blockchain<br />Developer</p>
+                    </Col>
+                    <Col xs={12} sm={8} md={6} lg={4} xl={4}>
+                        <p className='team-member-name'>JACOB</p>
+                        <img className='team-member-avatar' src={JACOB} alt="JACOB" />
+                        <p className='team-member-title'>Visual<br />director</p>
+                    </Col>
+                </Row>
+            </div>
+        </div>
     );
 };
 
