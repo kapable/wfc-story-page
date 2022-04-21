58b47cbc4b9a23c56aacfa855b3af0c0f806b70b
kapable <skjoldenstore@gmail.com>
2022-04-21 16:54:52 +0900

    change nft slide method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './Minting.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainStory from './pages/MainStory';
 import Minting from './pages/Minting';
--- src/components/NFT.js
+++ src/components/NFT.js
@@ -1,30 +1,22 @@
 import React, { Fragment } from 'react';
 import { NFTFOX1, NFTFOX2, NFTFOX3, NFTFOX4, NFTFOX5, NFTFOX6, NFTBACKGROUND, OPENSEABTN } from '../images';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Marquee from "react-fast-marquee";
 
 function NFT() {
-    const imgList = [NFTFOX1, NFTFOX2, NFTFOX3, NFTFOX4, NFTFOX5, NFTFOX6, NFTFOX1, NFTFOX2, NFTFOX3, NFTFOX4, NFTFOX5, NFTFOX6, NFTFOX1, NFTFOX2, NFTFOX3, NFTFOX4, NFTFOX5, NFTFOX6];
+    const imgList = [NFTFOX1, NFTFOX2, NFTFOX3, NFTFOX4, NFTFOX5, NFTFOX6];
     
     return (
         <Fragment>
             <div className='nft-background-img-div'>
                 <p className='nft-title'>W.F.C <span className='nft-title-bold'>NFT</span></p>
                 <img src={NFTBACKGROUND} alt="nft-background-img" className='nft-background-img' />
-                {/* <Slider {...settings}>
+                <Marquee className='nft-marquee-slider' gradient={false} speed={40} pauseOnHover={true}>
                     {imgList.map((v, i) => (
-                        <img src={v} key={`nft-fox-${i}`} alt={`nft-fox-${i}`} />
+                        <div className='nft-slide' key={`nft-fox-${i}-key`}>
+                            <img className='nft-slide-img' src={v} key={`nft-fox-${i}`} alt={`nft-fox-${i}`} />
+                        </div> 
                     ))}
-                </Slider> */}
-                <div className='nft-slider'>
-                    <div className='nft-slider-track'>
-                        {imgList.map((v, i) => (
-                            <div className='nft-slide' key={`nft-fox-${i}-key`}>
-                                <img className='nft-slide-img' src={v} key={`nft-fox-${i}`} alt={`nft-fox-${i}`} />
-                            </div> 
-                        ))}
-                    </div>
-                </div>
+                </Marquee>
                 <a href='https://ktestone.com' target="_blank" rel="noreferrer noopener">
                     <img src={OPENSEABTN} alt="go-to-opensea" className='go-to-opensea-btn' />
                 </a>
