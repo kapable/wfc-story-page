5d9ce59cc539225e501b38e52638bf7180e3cdfe
kapable <skjoldenstore@gmail.com>
2022-04-24 12:03:19 +0900

    apply minting info component

--- src/components/Minting/MintingInfo.js
+++ src/components/Minting/MintingInfo.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { KALYTNLOGO } from '../../images/Minting';
 
 const MintingInfo = () => {
     return (
-        <div className='minting-info-div'>MintingInfo</div>
+        <div className='minting-info-div'>
+            <div className='minting-info-div-one'>
+                <div className='minting-info-div-one-left'>
+                    <p className='minting-info-div-one-left-title'>PER TRANSACTION</p>
+                </div>
+                <p className='minting-info-div-one-right'>최대 3개</p>
+            </div>
+            <div className='minting-info-div-two'>
+                <div className='minting-info-div-two-left'>
+                    <p className='minting-info-div-two-left-title'>PRICE</p>
+                </div>
+                <p className='minting-info-div-two-right'><img className='minting-info-kalytn-logo' src={KALYTNLOGO} alt="klaytn-logo" /> 300 KLAY</p>
+            </div>
+            <div className='minting-info-div-three'>
+                <div className='minting-info-div-three-left'>
+                    <p className='minting-info-div-three-left-title'>PER WALLET</p>
+                </div>
+                <p className='minting-info-div-three-right'>Unlimited</p>
+            </div>
+        </div>
     );
 };
 
