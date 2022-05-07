f6abaa7ad85cb2044ea4c5e6da0d07a2049850d0
kapable <skjoldenstore@gmail.com>
2022-05-07 16:29:25 +0900

    apply minting amount control func

--- src/components/Minting/MintingAmount.js
+++ src/components/Minting/MintingAmount.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import ProgressBar from './ProgressBar';
 
 const MintingAmount = () => {
+    const [amount, setAmount] = useState(1);
+
+    const minusCount = useCallback(() => {
+        if(amount <= 1) {
+            alert("최소한 1개 이상 민팅에 참여해야 합니다!");
+            return null;
+        }
+        setAmount((amount) => amount - 1);
+    }, [amount]);
+
+    const plusCount = useCallback(() => {
+        if(amount >= 3) {
+            alert("최대 3개까지 민팅이 가능합니다!");
+            return null;
+        }
+        setAmount((amount) => amount + 1);
+    }, [amount]);
+
     return (
         <div className='minting-amount-div'>
             <div className='minting-amount-left-div'>
@@ -13,11 +31,11 @@ const MintingAmount = () => {
                 </div>
             </div>
             <div className='minting-amount-control-div'>
-                <div className='minting-amount-control-button-minus'>
+                <div className='minting-amount-control-button-minus' onClick={minusCount}>
                     <p className='minting-amount-control-button-minus-text'>-</p>
                 </div>
-                <div className='minting-amount-control-number'>1</div>
-                <div className='minting-amount-control-button-plus'>
+                <div className='minting-amount-control-number'>{amount}</div>
+                <div className='minting-amount-control-button-plus' onClick={plusCount}>
                     <p className='minting-amount-control-button-plus-text'>+</p>
                 </div>
             </div>
--- src/components/Minting/MintingButton.js
+++ src/components/Minting/MintingButton.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 const MintingButton = () => {
+    const onBtnClick = useCallback(() => {
+        alert("민팅 기능 준비중입니다!")
+    }, []);
+    
     return (
-        <div className='minting-button-div'>
+        <div className='minting-button-div' onClick={onBtnClick}>
             <p className='minting-button-text'>MINTING</p>
         </div>
     );
